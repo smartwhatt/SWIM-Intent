8109bd1ed9da611e6d570876bd5ab0b3c9703cec
Jimmy_Tempest <smartwattana@gmail.com>
2021-11-03 22:29:37 +0700

    seperate training data into modules

--- classification.py
+++ classification.py
@@ -13,7 +13,7 @@ from nltk.stem.lancaster import LancasterStemmer
 from numpy.__config__ import show
 
 from utils.preprocess import load_data, create_training_data, bag_of_words
-
+from utils.model import create_model, train_model
 # stemmer = LancasterStemmer()
 
 # Load dataset
@@ -23,40 +23,12 @@ docs_x, docs_y = docs
 # create trainning data
 training, output = create_training_data(words, labels, docs_x, docs_y)
 
-# training = []
-# output = []
-# out_empty = [0 for _ in range(len(labels))]
-# for x, doc in enumerate(docs_x):
-#     bag = []
-#     wrds = [stemmer.stem(w.lower()) for w in doc]
-#     for w in words:
-#         if w in wrds:
-#             bag.append(1)
-#         else:
-#             bag.append(0)
-#     output_row = out_empty[:]
-#     output_row[labels.index(docs_y[x])] = 1
-#     training.append(bag)
-#     output.append(output_row)
-# training = numpy.array(training)
-# output = numpy.array(output)
 with open("data.pickle", "wb") as f:
     pickle.dump((words, labels, training, output), f)
 
 
 # model creation
-model = Sequential([
-    Dense(8, input_shape=(len(training[0]), ), activation="relu"),
-    Dense(8, activation="relu"),
-    Dense(len(output[0]), activation="softmax")
-])
-
-loss = keras.losses.BinaryCrossentropy(from_logits=False)
-optim = tf.optimizers.Adam(lr=0.0083)
-metrics = ["accuracy"]
-
-# print("\nCompiling Model...\n")
-model.compile(loss=loss, optimizer=optim, metrics=metrics)
+model = create_model(len(training[0]), len(output[0]))
 
 model.fit(training, output, epochs=1000,  verbose=2, validation_split=0.2)
 model.save(f"model/model.h5")
